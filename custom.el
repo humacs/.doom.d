03c498a83709181f425dc3c889ddaf8934249713
Zach Mandeville <zz@ii.coop>
2021-01-12 14:16:22 +1300

    Create custom.el

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-javascript-standard-executable "SEMISTANDARD")
+ '(org-babel-clojure-backend (quote cider))
+ '(safe-local-variable-values
+   (quote
+    ((cider-default-cljs-repl . shadow)
+     (cider-shadow-cljs-default-options . "app")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Menlo" :foundry "nil" :slant normal :weight normal :height 200 :width normal)))))
